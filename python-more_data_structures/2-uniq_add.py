e4dd3994e1a71e6aa3c3423ac49cb0fe0d3cba2d
nawfel83 <laklitnawfel1@gmail.com>
2025-05-14 18:02:28 +0200

    refactor: optimize uniq_add function for summing unique integers

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    uniq_int = set(my_list)
-    count = 0
-    for i in uniq_int:
-        count += i
-    return count
+    result = 0
+    new_set = set(my_list)
+    for i in new_set:
+        result += i
+    return result
