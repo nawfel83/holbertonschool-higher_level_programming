1d9547437910c6941ad8b5677b4506c0f6b40c10
nawfel83 <laklitnawfel1@gmail.com>
2025-06-27 17:52:20 +0200

    add script to create MySQL user user_0d_1 with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates the MySQL user user_0d_1 with all privileges and password user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
