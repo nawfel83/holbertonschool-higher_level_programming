069860e1792be555b08d71ba4fb148f644b169f0
nawfel83 <laklitnawfel1@gmail.com>
2025-06-27 17:55:44 +0200

    fix comment formatting in privileges script

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- This script lists all privileges for the MySQL users user_0d_1 and user_0d_2 on localhost
+-- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
